96047497e5fa902ac9fe5bb7b32b31491787c318
adam <bcuz@users.noreply.github.com>
2019-01-31 23:17:00 -0500

    starting

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// https://via.placeholder.com/876x182
+
+let introImg = document.querySelector('.intro img');
+
+introImg.addEventListener('mouseover', (event) => {
+  introImg.src = 'https://via.placeholder.com/1000x300'
+})
