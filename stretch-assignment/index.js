56eb33cfa5f0745b20dc0e015a474592523986c6
adam <bcuz@users.noreply.github.com>
2019-02-02 13:39:16 -0500

    boxes blasting off but issues on 2nd click of same box

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,9 +1,29 @@
 document.querySelector('.blocks').addEventListener('click', event => {
 
-  
   if (event.target.tagName === 'DIV') {
     let clickedClass = event.target.className.split(" ")[1]  
     let clickedBox = document.querySelector(`.${clickedClass}`);
+    clickedBox.style.left = 0;
     clickedBox.parentNode.prepend(clickedBox);
   }
 })
+
+document.querySelector('.blocks').addEventListener('mousedown', event => {
+  
+  if (event.target.tagName === 'DIV') {
+    timer=setInterval(function(){
+        
+      event.target.style.position = 'relative'
+      
+      event.target.style.left = parseInt(event.target.style.left, 10) + 25 + "px"
+      
+
+    }, 100); 
+  }
+})
+
+document.querySelector('.blocks').addEventListener("mouseup", function(){
+  if (event.target.tagName === 'DIV') {
+    if (timer) clearInterval(timer)
+  }
+});
