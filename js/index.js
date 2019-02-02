ac75e1b5aaf65ea6131655e831a5bd52ba9debba
adam <bcuz@users.noreply.github.com>
2019-02-02 12:36:39 -0500

    last 2 unique listeners

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
-// Your code goes here
-
 let introImg = document.querySelector('.intro img');
 
+// 1
 introImg.addEventListener('mouseover', (event) => {
   event.target.src = 'https://via.placeholder.com/1000x300'
 })
 
+// 2
 introImg.addEventListener('mouseleave', (event) => {
   event.target.src = 'img/fun-bus.jpg'
 })
 
+// 3
 introImg.addEventListener('drag', (event) => {
   // event.target.src = 'img/fun-bus.jpg'
   document.querySelector('header').style.backgroundColor = 'orange'
 })
 
-
+// 4
 document.addEventListener('keydown', event => {
   if (event.keyCode == 27) {
     alert ("You pressed the Escape key!");
   }
 })
 
-// chain
+// 5
+// elegantly giving all .btn elements a click event
 document.querySelector('.content-pick').addEventListener('click', event => {
   if (event.target.className === 'btn') {
     alert("You clicked a button!");
@@ -44,10 +46,12 @@ let h1 = document.querySelector("h1")
 // make the h1 the first thing that's focused on
 h1.setAttribute("tabindex", "0")
 
+// 6
 h1.addEventListener('focus', event => {
   event.target.style.color = 'red'
 })
 
+// 7
 h1.addEventListener('blur', event => {
   event.target.style.color = '#000'
 })
@@ -57,4 +61,12 @@ document.querySelector(".img-content").addEventListener('dblclick', event => {
   event.target.style.transform = 'rotate(180deg)'
 })
 
-// resize	
+// 9
+window.addEventListener('resize', () => {
+  console.log('resizing');
+})
+
+// 10
+document.body.addEventListener('wheel', () => {
+  document.querySelector('.intro').style.backgroundColor = 'red'
+})
